c2953782032073df1c97479dfc0ed92bba9b686a
ProWithYou <32834452+ProWithYou@users.noreply.github.com>
2021-07-03 00:07:18 +0900

    Update M1.py

--- M1.py
+++ M1.py
@@ -2,13 +2,16 @@ import os
 
 # 입력
 # file_name = input("파일 이름을 입력하세요 : ")
-file_name = '502018850_KO_cnt_1'
+file_name = '502018850_KO_cnt_1.vtt'
 
 # 입력 파일 읽기
 file_data = open(file_name, 'r')
 
 # 파일 저장
 file_storage = file_data.read()
+
+# 문자열 바꾸기
+file_storage = file_storage.replace("line:90%,end position:50%,center align:center","")
 
 # 입력 파일 출력
 print(file_storage)
